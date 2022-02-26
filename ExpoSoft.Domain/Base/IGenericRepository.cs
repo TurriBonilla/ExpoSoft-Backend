5ee64d61d4495f8739c88cc63f32fa3ee9853007
ArnoldFrb <arnoldfrb@gmail.com>
2022-02-25 21:42:40 -0500

    Creation of the domain base
    Classes and interfaces BaseEntity, Entity, IEntity, IAggregateRoot and IGenericRepository were created

---
+++ ExpoSoft.Domain/Base/IGenericRepository.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExpoSoft.Domain.Base
+{
+    interface IGenericRepository<T> where T : BaseEntity
+    {
+        T Find(object id);
+        
+        void Add(T entity);
+        
+        void Delete(T entity);
+        
+        void Edit(T entity);
+
+        void AddRange(List<T> entities);
+
+        void DeleteRange(List<T> entities);
+
+        IEnumerable<T> GetAll();
+
+        T FindFirstOrDefault(Expression<Func<T, bool>> predicate);
+
+        IEnumerable<T> FindBy(Expression<Func<T, bool>> predicate);
+
+        IEnumerable<T> FindBy(
+            Expression<Func<T, bool>> filter = null,
+            Func<IQueryable<T>,
+            IOrderedQueryable<T>> orderBy = null,
+            string includeProperties = ""
+         );
+    }
+}
