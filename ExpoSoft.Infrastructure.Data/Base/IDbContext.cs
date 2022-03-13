ccbf314459f290bd313842555621f048434afdbb
ArnoldFrb <arnoldfrb@gmail.com>
2022-03-12 19:04:26 -0500

    Creation of the base classes for the EntityFrameworkCore
    1) IDbContext.
    2) UnitOfWork.
    3) DbContextBase.
    4) GenericRepository

---
+++ ExpoSoft.Infrastructure.Data/Base/IDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+
+namespace ExpoSoft.Infrastructure.Data.Base
+{
+    public interface IDbContext
+    {
+        DbSet<T> Set<T>() where T : class;
+        EntityEntry Entry(object entity);
+        EntityEntry<TEntity> Entry<TEntity>(TEntity entity) where TEntity : class;
+        void SetModified(object entity);
+        int SaveChanges();
+    }
+}
