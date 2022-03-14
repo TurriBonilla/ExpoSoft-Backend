b46338a75f9e233c8d24fdcb64fb1ceafe6c0f79
ArnoldFrb <arnoldfrb@gmail.com>
2022-03-13 23:50:45 -0500

    Registration service refactoring

--- ExpoSoft.Aplication/BusinessServices/SignUpService.cs
+++ ExpoSoft.Aplication/BusinessServices/SignUpService.cs
@@ -17,23 +17,28 @@ namespace ExpoSoft.Aplication.BusinessServices
 
         public SignUpResponse SignIn(SignUpRequest request)
         {
-            var entity = _businessRepository.FindFirstOrDefault(ent => ent.Nit == request.NIT);
-            if(entity == null)
+            var entityNIT = _businessRepository.FindFirstOrDefault(ent => ent.Nit == request.NIT);
+            var entityEmail = _businessRepository.FindFirstOrDefault(ent => ent.Nit == request.NIT);
+            if (entityEmail == null)
             {
-                var newEntity = new Business();
-                var resEntity = newEntity.SignUp(request.Name, request.NIT, request.Email, request.Password);
-                if (resEntity.Equals("Registro Exitoso."))
+                if (entityEmail == null)
                 {
-                    _businessRepository.Add(newEntity);
-                    _unitOfWork.Commit();
-                    return new SignUpResponse(201, resEntity);
+                    var newEntity = new Business();
+                    var resEntity = newEntity.SignUp(request.Name, request.NIT, request.Email, request.Password);
+                    if (resEntity.Equals("Registro Exitoso."))
+                    {
+                        _businessRepository.Add(newEntity);
+                        _unitOfWork.Commit();
+                        return new SignUpResponse(201, resEntity, newEntity.Id);
+                    }
+                    return new SignUpResponse(400, resEntity, -1);
                 }
-                return new SignUpResponse(400, resEntity);
+                return new SignUpResponse(400, $"Ya existe una empresa con el correo {request.Email}.", -1);
             }
-            return new SignUpResponse(400, $"Ya existe una empresa con el NIT:{request.NIT}.");
+            return new SignUpResponse(400, $"Ya existe una empresa con el NIT:{request.NIT}.", -1);
         }
     }
 
     public record SignUpRequest(string NIT, string Name,string Email, string Password);
-    public record SignUpResponse(int Code, string Message);
+    public record SignUpResponse(int Code, string Message, int Token);
 }
